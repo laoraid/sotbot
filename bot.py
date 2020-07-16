fee91ecfb4753b9fbea80274574b26df74502936
Laoraid <a99azaz@gmail.com>
2020-07-16 16:55:38 +0900

    섬 검색 추가

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
 import os
+import difflib
+from difflib import SequenceMatcher
 from discord.ext import commands
 import discord
+import pandas as pd
 
 try:
     BOT_TOKEN = os.environ['BOT_TOKEN']
@@ -9,6 +12,9 @@ except KeyError:
     BOT_TOKEN = config.TOKEN
 
 bot = commands.Bot(command_prefix='!')
+islandspos = pd.read_excel('islands.xlsx',
+                            header=0,
+                            index_col=0).to_dict()['pos']
 
 
 @bot.command()
@@ -23,6 +29,30 @@ async def 아이디(ctx: commands.Context, *args):
     await ctx.send(f'{prevnick}의 닉네임이 {nick}으로 변경되었습니다.')
 
 
+@bot.command()
+async def 좌표(ctx, *args):
+    if (len(args) == 0):
+        await ctx.send('사용법 : ``!좌표`` ``섬 이름``')
+        return None
+    
+    args = [x.lower() for x in args]
+    island = ' '.join(args)
+    islands = list(islandspos.keys())
+
+    substr = [x for x in islands if island in x.lower()]
+
+    if (len(substr) != 0):
+        island = substr[0]
+    else:
+        clmat = difflib.get_close_matches(island, islands, cutoff=0.5)
+        if (len(clmat) == 0):
+            await ctx.send(f"``{island}`` 섬을 찾을 수 없습니다.")
+            return None
+        island = clmat[0]
+
+    await ctx.send(f"``{island}``의 좌표는 ``{islandspos[island]}``")
+
+
 @bot.event
 async def on_member_join(member):
     msg = (f'{member.mention} 디시인사이드 Sea of Thieves 갤러리 디스코드에 오신 걸 환영합니다.\n'
@@ -32,4 +62,8 @@ async def on_member_join(member):
     await ch.send(msg)
 
 print('봇 실행중')
+str1 = 'Sanctuary Outpost'
+str2 = 'santury'
+ratio = SequenceMatcher(None, str1, str2).ratio()
+print(ratio)
 bot.run(BOT_TOKEN)
