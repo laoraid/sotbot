4823ce9959627d07df44cb5895cb7527d89395f7
Laoraid <a99azaz@gmail.com>
2020-07-17 01:04:36 +0900

    좌표 검색 서순 변경

--- bot.py
+++ bot.py
@@ -13,8 +13,8 @@ except KeyError:
 
 bot = commands.Bot(command_prefix='!')
 islandspos = pd.read_excel('islands.xlsx',
-                            header=0,
-                            index_col=0).to_dict()['pos']
+                           header=0,
+                           index_col=0).to_dict()['pos']
 
 
 @bot.command()
@@ -43,22 +43,22 @@ async def 좌표(ctx, *args):
     if (len(args) == 0):
         await ctx.send('사용법 : ``!좌표`` ``섬 이름``')
         return None
-    
+
     args = [x.lower() for x in args]
     island = ' '.join(args)
     islands = list(islandspos.keys())
 
-    substr = [x for x in islands if island in x.lower()]
+    clmat = difflib.get_close_matches(island, islands, cutoff=0.5)
 
-    if (len(substr) != 0):
-        island = substr[0]
-    else:
-        clmat = difflib.get_close_matches(island, islands, cutoff=0.5)
-        if (len(clmat) == 0):
+    if (len(clmat) == 0):
+        substr = [x for x in islands if island in x.lower()]
+        if (len(substr) == 0):
             await ctx.send(f"``{island}`` 섬을 찾을 수 없습니다.")
             return None
+        else:
+            island = substr[0]
+    else:
         island = clmat[0]
-
     await ctx.send(f"``{island}``의 좌표는 ``{islandspos[island]}``")
 
 
