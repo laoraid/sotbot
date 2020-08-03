7c3c86849d2c93ecf7bdaa50909c4908c6f35dd0
Laoraid <a99azaz@gmail.com>
2020-08-03 20:28:33 +0900

    로그 추가, 에러 처리 방법 변경, 서버 처리 비동기로 변경

--- bot.py
+++ bot.py
@@ -3,18 +3,22 @@ import os
 import difflib
 import re
 import datetime
+import aiohttp
 
 from discord.ext import commands
 import discord
 
 import openpyxl
 
-import requests
 from bs4 import BeautifulSoup
 # endregion
 
+CMD_PREFIX = "!"
+HELP_PREFIX = f"``{CMD_PREFIX}"
 
-bot = commands.Bot(command_prefix='!')
+
+bot = commands.Bot(command_prefix=CMD_PREFIX)
+bot.owner_id = 226700060308668420
 
 # region excel parse
 sheet = openpyxl.load_workbook('./islands.xlsx', data_only=True)['Sheet1']
@@ -39,13 +43,9 @@ while (True):
 
 
 @bot.command(description="xbox 아이디가 포함되게 닉네임을 변경합니다.",
-             help="``!아이디`` ``xboxid``")
-async def 아이디(ctx: commands.Context, *args):
-    if (len(args) == 0):
-        await ctx.send('사용법 : ``!아이디`` ``xboxid``')
-        return None
+             help=f"{HELP_PREFIX}아이디`` ``xboxid``")
+async def 아이디(ctx, *, id):
     prevnick = ctx.author.display_name
-    id = " ".join(args)
 
     pattern = r'(?P<nickname>\S+)\s*\(.+\)'
 
@@ -61,14 +61,9 @@ async def 아이디(ctx: commands.Context, *args):
 
 
 @bot.command(aliases=['pos'], description="섬의 좌표와 위키 링크를 출력합니다.",
-             help='``!좌표`` ``섬 이름``')
-async def 좌표(ctx, *args):
-    if (len(args) == 0):
-        await ctx.send('사용법 : ``!좌표`` ``섬 이름``')
-        return None
-
-    args = [x.lower() for x in args]
-    island = ' '.join(args)
+             help=f"{HELP_PREFIX}좌표`` ``섬 이름``")
+async def 좌표(ctx, *, island):
+    island = island.lower()
 
     iseng = re.match(r'[^ㄱ-ㅎㅏ-ㅣ가-힣]', island)
     iseng = True if iseng is not None else False
@@ -111,7 +106,12 @@ lastserverstat = ""
 RELOAD_TIME = datetime.timedelta(minutes=3)
 
 
-@bot.command(description="서버 상태를 확인합니다. 쿨타임 3분", help="``!서버``")
+def dt_to_str(date: datetime.datetime):
+    return date.strftime("%Y-%m-%d %H:%M:%S")
+
+
+@bot.command(description="서버 상태를 확인합니다. 쿨타임 3분",
+             help=f"{HELP_PREFIX}서버``")
 async def 서버(ctx):
     STATURL = "https://www.seaofthieves.com/ko/status"
     SERVER_IS_SICK = ":regional_indicator_x: 점검중이거나 서버가 터졌어요."
@@ -119,62 +119,55 @@ async def 서버(ctx):
     SERVER_UNKNOWN = "서버 상태를 확인할 수 없어요."
 
     global lastserverstat, lastchktime
+    msg = None
 
     if lastchktime is None:
         cachetime = RELOAD_TIME
     else:
         cachetime = datetime.datetime.now() - lastchktime
 
-    if cachetime < RELOAD_TIME:
-        await ctx.send(lastserverstat)
-        return None
+    if not cachetime < RELOAD_TIME:
+        msg = await ctx.send("서버 상태 확인중...")
+        async with aiohttp.ClientSession() as session:
+            async with session.get(STATURL) as res:
+                lastchktime = datetime.datetime.now()
 
-    res = requests.get(STATURL)
+                if res.status == 200:
+                    soup = BeautifulSoup(await res.text(), 'html.parser')
+                elif res.status != 200 or res is None:
+                    lastserverstat = SERVER_UNKNOWN
+                    await ctx.send(lastserverstat)
+                    return
 
-    if res.status_code != 200 or res is None:
-        await ctx.send(SERVER_UNKNOWN)
-        return None
+        srvinfo = soup.find("div", {"class": "service__info"})
+        text = srvinfo.find("h2").contents[0]
 
-    soup = BeautifulSoup(res.text, 'html.parser')
+        if text == "문제가 발생하여 서비스에 지장이 있습니다.":
+            lastserverstat = SERVER_IS_SICK
+        if text == "모든 서비스가 정상 운영되고 있습니다.":
+            lastserverstat = SERVER_HEALTHY
+        else:
+            lastserverstat = SERVER_UNKNOWN
 
-    srvinfo = soup.find("div", {"class": "service__info"})
-    text = srvinfo.find("h2").contents[0]
-    lastchktime = datetime.datetime.now()
+    lctstr = dt_to_str(lastchktime)
 
-    if text == "문제가 발생하여 서비스에 지장이 있습니다.":
-        await ctx.send(SERVER_IS_SICK)
-        lastserverstat = SERVER_IS_SICK
-    if text == "모든 서비스가 정상 운영되고 있습니다.":
-        await ctx.send(SERVER_HEALTHY)
-        lastserverstat = SERVER_HEALTHY
-    else:
-        await ctx.send(SERVER_UNKNOWN)
-        lastserverstat = SERVER_UNKNOWN
+    if msg is not None:
+        await msg.delete()
 
-"""
-@bot.event
-async def on_member_join(member):
-    msg = (f'{member.mention} ``!아이디`` ``xboxid`` 로 게임 내 아이디 지정하고\n'
-           '규칙은 https://discord.gg/QhHZJUH 보고 겜하셈')
-    ch = bot.get_channel(726374943121473627)
-    await ch.send(msg)
-"""
+    await ctx.send(f"{lastserverstat} 확인 시간 : ``{lctstr}``")
+
 
 bot.remove_command('help')
 
 
-@bot.command(aliases=['help'],description='이 도움말을 출력합니다.')
-async def 도움말(ctx, *args):
-    if len(args) == 0:
+@bot.command(aliases=['help'], description='이 도움말을 출력합니다.')
+async def 도움말(ctx, cmd):
+    if cmd == 'help' or cmd == "도움말":
         await ctx.send(embed=make_help_embed())
+    elif cmd in viscomsdict:
+        await ctx.send(f"{viscomsdict[cmd].help}")
     else:
-        cn = ''.join(args)
-        if cn == 'help':
-            await ctx.send(embed=make_help_embed())
-        elif cn in viscomsdict:
-            await ctx.send(viscomsdict[cn].help)
-        else:
-            await ctx.send(f'``{cn}`` 명령어를 찾을 수 없습니다.')
+        await ctx.send(f'``{cmd}`` 명령어를 찾을 수 없습니다.')
 
 
 def make_help_embed():
@@ -186,9 +179,23 @@ def make_help_embed():
         arr.extend(c.aliases)
         embed.add_field(name=', '.join(arr), value=c.description)
 
-    embed.set_footer(text="'!help 명령어' 입력시 개별 명령어 도움말 출력")
+    embed.set_footer(text=f"'{CMD_PREFIX}help 명령어' 입력시 개별 명령어 도움말 출력")
     return embed
 # endregion
+
+# region logging
+
+@bot.before_invoke
+async def log_i(ctx: commands.Context):
+    print(f"[info/{ctx.message.created_at}] {ctx.author} | {ctx.message.content}")
+
+
+def log_e(ctx, error):
+    now = dt_to_str(datetime.datetime.now())
+    print(f"[error/{now}] | {ctx.message.content} | {error}")
+#endregion
+
+#region events
 
 
 @bot.event
@@ -202,9 +209,24 @@ async def on_message(msg):
 
 @bot.event
 async def on_ready():
-    await bot.change_presence(activity=discord.Game(name='!help'))
+    await bot.change_presence(activity=discord.Game(name=f'{CMD_PREFIX}help'))
     print(f'봇 실행중 서버 : {len(bot.guilds)}개')
 
+
+@bot.event
+async def on_command_error(ctx, error):
+    await log_i(ctx)
+    if isinstance(error, commands.MissingRequiredArgument):
+        if ctx.command.name == "도움말":
+            await ctx.send(embed=make_help_embed())
+        else:
+            await ctx.send(f"사용법 : {ctx.command.help}")
+    else:
+        log_e(ctx, error)
+        owner = f"<@!{bot.owner_id}>"
+        await ctx.send(f"에러가 발생했습니다. {owner}")
+
+#endregion
 
 try:
     BOT_TOKEN = os.environ['BOT_TOKEN']
